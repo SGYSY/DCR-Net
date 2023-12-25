6e01a9d3270c4b6a1fec580e9def37580ff5f83f
SGYSY <Siyuyanyanyan@gmail.com>
2023-12-25 18:48:53 +0800

    finish the bilstm for D

--- nn/relation.py
+++ nn/relation.py
@@ -8,10 +8,26 @@ class MLP(nn.Module):
                  input_dim: int,
                  hidden_dim: int):
         super().__init__()
-        self.l1 = nn.Linear(input_dim, hidden_dim)
-        self.l2 = nn.Linear(hidden_dim, input_dim)
+        self._mlp1_cell = nn.Linear(input_dim, hidden_dim)
+        self._mlp2_cell = nn.Linear(hidden_dim, input_dim)
 
     def forward(self, S):
-        S = F.relu(self.l1(S))
-        S = self.l2(S)
-        return S
+        relu_S = F.relu(self._mlp1_cell(S))
+        output_S = self._mlp2_cell(relu_S)
+        return output_S
+
+
+class BiLSTM(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 hidden_dim: int,
+                 dropout_rate: float):
+        super().__init__()
+        self._rnn_cell = nn.LSTM(input_dim, hidden_dim // 2, bidirectional=True, batch_first=True)
+
+    def forward(self, D):
+        output_D, _= self._rnn_cell(D)
+        return output_D
+
+
+
