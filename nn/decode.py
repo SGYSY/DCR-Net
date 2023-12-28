311730eb928830c34b0dd3646609581a382e9927
SGYSY <Siyuyanyanyan@gmail.com>
2023-12-28 20:14:18 +0800

    add some layers

--- nn/decode.py
+++ nn/decode.py
@@ -1,13 +1,26 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+from nn.relation import CoInteractiveRelation
+
 
 class RelationDecoder(nn.Module):
     def __init__(self,
                  hidden_dim: int,
                  num_dar: int,
-                 num_sc: int):
+                 num_sc: int,
+                 dropout_rate: float,
+                 input_dim: int,
+                 num_layer: int):
         super().__init__()
+
+        self._num_layer = num_layer
+
+        self._sent_layer_dict = nn.ModuleDict()
+        self._act_layer_dict = nn.ModuleDict()
+
+        self._relate_layer = CoInteractiveRelation(input_dim, hidden_dim, dropout_rate)
+
         self.dar_linear = nn.Linear(hidden_dim, num_dar)
         self.sc_linear = nn.Linear(hidden_dim, num_sc)
 
@@ -17,7 +30,7 @@ class RelationDecoder(nn.Module):
         sc_linear = self.sc_linear(SL)
 
         # Softmax for probability distributions
-        dar_prob = F.softmax(dar_linear)
-        sc_prob = F.softmax(sc_linear)
+        dar_prob = F.softmax(dar_linear, dim=-1)
+        sc_prob = F.softmax(sc_linear, dim=-1)
 
         return dar_prob, sc_prob
--- nn/relation.py
+++ nn/relation.py
@@ -3,47 +3,83 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class StackedRelation(nn.Module):
+    def __init__(self,
+                 input_dim: int,
+                 hidden_dim: int,
+                 dropout_rate: float,
+                 n_layer=3):
+        super().__init__()
+
+        self.layer = n_layer
+        self.dropout = dropout_rate
+        self.first_layer = CoInteractiveRelation(input_dim, hidden_dim, dropout_rate)
+        self.second_layer = CoInteractiveRelation(hidden_dim, hidden_dim, dropout_rate)
+        self.third_layer = CoInteractiveRelation(hidden_dim, hidden_dim, dropout_rate)
+
+    def add_missing_arg(self, layer=3):
+        self.layer = layer
+
+    def forward(self, S, D):
+        S, D = self.first_layer(S, D)
+
+        if self.layer > 1:
+            S, D = self.second_layer(S, D)
+        if self.layer > 2:
+            S, D = self.third_layer(S, D)
+
+        return S, D
+
+
 class CoInteractiveRelation(nn.Module):
     def __init__(self,
                  input_dim: int,
                  hidden_dim: int,
-                 dropout):
+                 dropout_rate: float,
+                 n_layer=3):  # 还有一些其他参数
         """
         Use MLP + Co-Attention to Encode
         """
 
         super().__init__()
-        self.dropout = dropout
-        self._mlp = MLP(input_dim, hidden_dim, dropout=dropout)
-        self._lstm = BiLSTM(input_dim, hidden_dim, dropout=dropout)
-        self._mlp_layer = MLPLayer(input_dim, hidden_dim, dropout=dropout)
-        self._co_attention = CoAttention(dropout=dropout)
+
+        self.dropout = dropout_rate
+        self.layer = n_layer
+
+        self._mlp = MLP(input_dim, hidden_dim, dropout=dropout_rate)
+        self._lstm = BiLSTM(input_dim, hidden_dim, dropout=dropout_rate)
+        self._mlp_layer = MLPLayer(2 * hidden_dim, hidden_dim, dropout=dropout_rate)
+        self._co_attention = CoAttentionLayer(dropout=dropout_rate)  # 输入维度 输出维度 alpha concat这些都没有处理
 
     def forward(self, S, D):
-        S = self._mlp(S)
-        D = self._lstm(D)
+        pre_s = self._mlp(S)
+        pre_d = self._lstm(D)
 
-        mlp_D = self._mlp_layer(D)
-        mlp_S = self._mlp_layer(S)
+        mlp_s, mlp_d = self._mlp_layer(pre_s, pre_d)
 
-        co_D, co_S = self._co_attention(S, D)
+        co_s, co_d = self._co_attention(mlp_s, mlp_d)  # 最后输出需要用elu激活函数吗
 
-        return mlp_D, mlp_S, co_D, co_S
+        return co_s, co_d
 
 
-class CoAttention(nn.Module):
-    def __init__(self, dropout):
+class CoAttentionLayer(nn.Module):
+    def __init__(self, dropout: float):
         super().__init__()
-        self.dropout = dropout
+        self.dropout = nn.Dropout(dropout)
+        # Define dimensions for the linear layers based on your architecture
+        # Example: self.attention = nn.Linear(hidden_dim, hidden_dim)
 
-    def forward(self, S, D):
-        attention_D = F.softmax(torch.matmul(D, S.transpose(1, 2)), dim=-1)
-        attention_S = F.softmax(torch.matmul(S, D.transpose(1, 2)), dim=-1)
+    def forward(self, S, D):  # 可能不需要dropout
+        drop_s = self.dropout(S)
+        drop_d = self.dropout(D)
 
-        co_D = D + torch.matmul(attention_D, S)
-        co_S = S + torch.matmul(attention_S, D)
+        attention_d = F.softmax(torch.matmul(drop_d, drop_s.transpose(1, 2)), dim=-1)
+        attention_s = F.softmax(torch.matmul(drop_s, drop_d.transpose(1, 2)), dim=-1)
 
-        return co_D, co_S
+        co_d = drop_d + torch.matmul(attention_d, drop_s)
+        co_s = drop_s + torch.matmul(attention_s, drop_d)  # 最后输出需要用elu激活函数吗
+
+        return co_s, co_d
 
 
 class MLPLayer(nn.Module):
@@ -54,14 +90,14 @@ class MLPLayer(nn.Module):
         super().__init__()
         self.mlp_layer = MLP(input_dim, hidden_dim, dropout=dropout)
 
-    def forward(self, prime_S, prime_D):
-        cat_D = torch.cat((prime_S, prime_D), dim=-1)
-        cat_S = torch.cat((prime_S, prime_D), dim=-1)
+    def forward(self, prime_s, prime_d):
+        cat_d = torch.cat((prime_s, prime_d), dim=-1)
+        cat_s = torch.cat((prime_s, prime_d), dim=-1)
 
-        mlp_D = self.mlp_layer(cat_D)
-        mlp_S = self.mlp_layer(cat_S)
+        mlp_d = self.mlp_layer(cat_d)
+        mlp_s = self.mlp_layer(cat_s)
 
-        return mlp_D, mlp_S
+        return mlp_s, mlp_d
 
 
 class MLP(nn.Module):
@@ -75,10 +111,10 @@ class MLP(nn.Module):
         self._dropout = nn.Dropout(dropout)
 
     def forward(self, S):
-        relu_S = F.relu(self._mlp1_cell(S))
-        drop_S = self._dropout(relu_S)
-        output_S = self._mlp2_cell(drop_S)
-        return output_S
+        relu_s = F.relu(self._mlp1_cell(S))
+        drop_s = self._dropout(relu_s)
+        output_s = self._mlp2_cell(drop_s)
+        return output_s
 
 
 class BiLSTM(nn.Module):
@@ -91,5 +127,5 @@ class BiLSTM(nn.Module):
                                  batch_first=True, dropout=dropout)
 
     def forward(self, D):
-        output_D, _ = self._rnn_cell(D)
-        return output_D
+        output_d, _ = self._rnn_cell(D)
+        return output_d
